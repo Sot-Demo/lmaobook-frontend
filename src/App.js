d4915bdfe8b1970ef583e475744e589574196092
Will Seagar <w.seagar@gmail.com>
2020-08-06 19:14:14 +1200

    add frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+const API_URL = "https://lmaobook-backend.herokuapp.com";
+
 function App() {
+
+  const [name, setName] = useState();
+  const [message, setMessage] = useState();
+
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments();
+
+  }, []);
+
+  const getComments = () => {
+    fetch(API_URL + '/comments')
+      .then((response) => response.json())
+      .then((data) => setComments(data))
+  }
+
+  const saveComment = () => {
+    const comment = {
+      name: name,
+      message: message
+    }
+
+    fetch(API_URL + "/createComment", {
+      method: "POST",
+      headers : { "content-type": "application/json"},
+      body: JSON.stringify(comment)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          setName("");
+          setMessage("");
+          getComments();
+        }
+      })
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Message</p>
+      <input onChange={(e) => setMessage(e.target.value)}></input>
+      <p>Name</p>
+      <input onChange={(e) => setName(e.target.value)}></input>
+      <button onClick={saveComment}>Submit</button>
+      {comments.map((comment) => {
+
+      return (
+        <>
+          <h1>{comment.name}</h1>
+          <p>{comment.message}</p>
+        </>
+      )
+
+    })}
+    </>
   );
 }
 
